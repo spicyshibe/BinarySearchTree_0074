67033f3cbd9a2eec09379de0e594eace8026177a
spicyshibe <rafierasyw@gmail.com>
2025-06-26 18:23:12 +0700

    constructor untuk class node

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class Node
+{
+    public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Constructor for the node class
+    Node()
+    {
+
+
+    }
+};
+
