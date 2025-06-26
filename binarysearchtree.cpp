54fe4c00292e9ed0b15a485671b53beaad5cf708
spicyshibe <rafierasyw@gmail.com>
2025-06-26 18:32:57 +0700

    step 3

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -39,5 +39,9 @@ class BinaryTree
         // step 2: Assign value to the data field of new node 
         newNode->info = x;
 
+        // step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
     }
 }
