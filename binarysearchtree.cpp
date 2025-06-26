f4ca634fd6904feaea6595d65cff6136354b4902
spicyshibe <rafierasyw@gmail.com>
2025-06-26 18:32:08 +0700

    step 1

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -26,4 +26,14 @@ class BinaryTree
     {
         ROOT = nullptr; // initializing ROOT to null
     }
+
+    void insert()
+    {
+         int x;
+        cout << "Masukkan nilai: ";
+        cin >> x;
+
+        // step 1: Allocate memory for the new node
+        Node *newNode = new Node();
+    }
 }
