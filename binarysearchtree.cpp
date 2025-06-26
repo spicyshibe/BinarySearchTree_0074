6ff4e465d41815c89ae0f291d29d4d874e858f43
spicyshibe <rafierasyw@gmail.com>
2025-06-26 18:41:35 +0700

    step 6a

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -62,7 +62,9 @@ class BinaryTree
          // step 6: If the value in the data field of new node is less than that of parent
         if (x < parent->info)
         {
-       
+            // step 6a. Make the left child of parent point to the new node
+            parent->leftchild = newNode;
+
         }
 
     }
